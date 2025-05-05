3d48c77c9de13fedc1284736fe811f7c734fde54
Ashok Cutkosky <ashok@cutkosky.com>
2025-05-05 11:22:10 -0400

    remove normopt

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -5,4 +5,3 @@ from optimizers.mango import Mango
 from optimizers.sfmuon import SFMuon
 from optimizers.adam import AdamW
 from optimizers.muon_err import MuonErr
-from optimizers.normopt import NormOpt
