bdd9fc6f9af61b90dc0d9f2ac0ff083ff4663f77
Ashok Cutkosky <ashok@cutkosky.com>
2025-09-08 21:50:21 +0000

    tune extra_update=0.01, smoothing coooldown=4000

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -436,7 +436,7 @@ adam_param_groups = [dict(params=head_params, lr=1/320), dict(params=embed_param
 # small adam epsilon by @YouJiacheng. this is an alternate method of fixing the world_size dependence
 # discovered by @fernbear.bsky.social https://x.com/hi_tysam/status/1879692937589875094
 optimizer1 = torch.optim.AdamW(adam_param_groups, betas=(0.8, 0.95), eps=1e-10, weight_decay=0.0, fused=True)
-optimizer2 = Muon(hidden_matrix_params, lr=0.025, momentum=0.95, update_smoothing=0.1, extra_update=0.1, rank=rank, world_size=world_size)
+optimizer2 = Muon(hidden_matrix_params, lr=0.025, momentum=0.95, update_smoothing=0.1, extra_update=0.01, rank=rank, world_size=world_size)
 optimizers: list[torch.optim.Optimizer] = [optimizer1, optimizer2]
 def opt_params(opt: torch.optim.Optimizer) -> list[nn.Parameter]:
     return [p for group in opt.param_groups for p in group["params"]]
@@ -552,7 +552,7 @@ for step in range(train_steps + 1):
         frac = min(step / 300, 1) # momentum warmup for muon
         group["momentum"] = (1 - frac) * 0.85 + frac * 0.95
         
-        smooth_frac = min(step / 2000, 1) # update_smoothing cooldown for muon
+        smooth_frac = min(step / 4000, 1) # update_smoothing cooldown for muon
         group["update_smoothing"] = (1 - smooth_frac) * 0.3 + smooth_frac * 0.1
     # step the optimizers
     for opt in optimizers:
