6ae716fa0d794ad14f33e831b5c89fea1a1f311c
Ashok Cutkosky <ashok@cutkosky.com>
2025-09-09 14:11:05 +0000

    try large update_smooth=.99 and large extra_step=0.99

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -356,7 +356,7 @@ class Hyperparameters:
     train_seq_len = 64*1024 # FlexAttention sequence length
     val_seq_len = 4*64*1024 # FlexAttention sequence length for validation
     # optimization
-    num_iterations = 5720 # number of iterations to run
+    num_iterations = 5750 # number of iterations to run
     cooldown_frac = 0.7 # fraction of training spent cooling down the learning rate
     # architecture
     vocab_size = 50257
@@ -436,7 +436,7 @@ adam_param_groups = [dict(params=head_params, lr=1/320), dict(params=embed_param
 # small adam epsilon by @YouJiacheng. this is an alternate method of fixing the world_size dependence
 # discovered by @fernbear.bsky.social https://x.com/hi_tysam/status/1879692937589875094
 optimizer1 = torch.optim.AdamW(adam_param_groups, betas=(0.8, 0.95), eps=1e-10, weight_decay=0.0, fused=True)
-optimizer2 = Muon(hidden_matrix_params, lr=0.025, momentum=0.95, update_smoothing=0.1, extra_update=0.0, rank=rank, world_size=world_size)
+optimizer2 = Muon(hidden_matrix_params, lr=0.025, momentum=0.95, update_smoothing=0.1, extra_update=0.99, rank=rank, world_size=world_size)
 optimizers: list[torch.optim.Optimizer] = [optimizer1, optimizer2]
 def opt_params(opt: torch.optim.Optimizer) -> list[nn.Parameter]:
     return [p for group in opt.param_groups for p in group["params"]]
@@ -553,7 +553,7 @@ for step in range(train_steps + 1):
         group["momentum"] = (1 - frac) * 0.85 + frac * 0.95
         
         smooth_frac = min(step / 2000, 1) # update_smoothing cooldown for muon
-        group["update_smoothing"] = (1 - smooth_frac) * 0.3 + smooth_frac * 0.1
+        group["update_smoothing"] = (1 - smooth_frac) * 0.9 + smooth_frac * 0.99
     # step the optimizers
     for opt in optimizers:
         torch.futures.collect_all(opt2futures[opt]).wait()
