aa700ba44da12010288b8209bf6438178d322876
Ashok Cutkosky <ashok@cutkosky.com>
2025-09-19 23:58:10 +0000

    fix bug in smoothing frac

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -916,7 +916,7 @@ for step in range(train_steps + 1):
         group["momentum"] = (1 - frac) * 0.85 + frac * 0.95
         
         smoothing_frac = min(step / 3000, 1)  # update_smoothing cooldown for muon
-        group["update_smoothing"] = (1 - frac) * 0.5 + frac * 0.2
+        group["update_smoothing"] = (1 - smoothing_frac) * 0.5 + smoothing_frac * 0.2
     # step the optimizers
     for opt in inner_optimizers:
         torch.futures.collect_all(opt2futures[opt]).wait()
