6882825ca79e4216a3f1a65044ade0b136e51132
Ashok Cutkosky <ashok@cutkosky.com>
2025-09-22 16:25:47 +0000

    smoothing 0.4->0.2 over 3000, 5600 iters, 0.01 final lr, 0.03 muon lr

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -914,7 +914,9 @@ for step in range(train_steps + 1):
     for group in inner_hidden_optim.param_groups:
         frac = min(step / 300, 1)  # momentum warmup for muon
         group["momentum"] = (1 - frac) * 0.85 + frac * 0.95
-        group["update_smoothing"] = (1 - frac) * 0.5 + frac * 0.2
+
+        smoothing_anneal_frac = min(step / 3000, 1) # smoothing
+        group["update_smoothing"] = (1 - smoothing_anneal_frac) * 0.4 + smoothing_anneal_frac * 0.2
     # step the optimizers
     for opt in inner_optimizers:
         torch.futures.collect_all(opt2futures[opt]).wait()
